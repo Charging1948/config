e809a5cc771d4290504975e9cc0da1ca43987f4a
jk <joachim.dev@pm.me>
2024-03-06 08:24:09 +0100

    refactor keyboard module

--- modules/nixos/hardware/keyboard/default.nix
+++ modules/nixos/hardware/keyboard/default.nix
@@ -1,22 +1,28 @@
-{ options, config, pkgs, lib, ... }:
-
-with lib;
-with lib.plusultra;
-let cfg = config.plusultra.hardware.keyboard;
-in
 {
+  options,
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with lib;
+with lib.plusultra; let
+  cfg = config.plusultra.hardware.keyboard;
+in {
   options.plusultra.hardware.keyboard = with types; {
     enable = mkBoolOpt false "Whether or not to enable keyboard support.";
-    enableZsa = mkBoolOpt false "Whether or not to enable zsa-keyboard support.";
-    enableWooting = mkBoolOpt false "Whether or not to enable wooting-keyboards support.";
+    zsa.enable =
+      mkBoolOpt false "Whether or not to enable zsa-keyboard support.";
+    wooting.enable =
+      mkBoolOpt false "Whether or not to enable wooting-keyboards support.";
   };
 
   config = mkIf cfg.enable {
     hardware = {
-      keyboard.zsa = mkIf cfg.enableZsa enabled;
-      wooting = mkIf cfg.enableWooting enabled;
+      keyboard.zsa = mkIf cfg.zsa.enable enabled;
+      wooting = mkIf cfg.wooting.enable enabled;
     };
-    plusultra.user.extraGroups = mkIf cfg.enableWooting [ "input" ];
-    environment.systemPackages = mkIf cfg.enableWooting [ pkgs.wootility ];
+    plusultra.user.extraGroups = mkIf cfg.wooting.enable ["input"];
+    environment.systemPackages = mkIf cfg.wooting.enable [pkgs.wootility];
   };
 }
