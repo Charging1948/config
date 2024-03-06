645bddc2fd3a190f8bd89abb2de0696626c1aff5
jk <joachim.dev@pm.me>
2024-03-06 16:27:49 +0100

    refactor plymouth option

--- modules/nixos/system/boot/default.nix
+++ modules/nixos/system/boot/default.nix
@@ -1,36 +1,41 @@
-{ options, config, pkgs, lib, ... }:
+{
+  options,
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 with lib;
-with lib.plusultra;
-let cfg = config.plusultra.system.boot;
+with lib.plusultra; let
+  cfg = config.plusultra.system.boot;
 in {
   options.plusultra.system.boot = with types; {
     enable = mkBoolOpt false "Whether or not to enable booting.";
-    enablePlymouth = mkBoolOpt false "Whether or not to enable Plymouth.";
+    plymouth.enable = mkBoolOpt false "Whether or not to enable Plymouth.";
   };
 
   config = mkIf cfg.enable {
-    boot = {
+    boot =
+      {
+        loader = {
+          efi.canTouchEfiVariables = true;
 
-      loader = {
+          systemd-boot = {
+            enable = true;
+            configurationLimit = 10;
 
-        efi.canTouchEfiVariables = true;
-
-        systemd-boot = {
-
-          enable = true;
-          configurationLimit = 10;
-
-          # https://github.com/NixOS/nixpkgs/blob/c32c39d6f3b1fe6514598fa40ad2cf9ce22c3fb7/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix#L66
-          editor = false;
+            # https://github.com/NixOS/nixpkgs/blob/c32c39d6f3b1fe6514598fa40ad2cf9ce22c3fb7/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix#L66
+            editor = false;
+          };
+        };
+      }
+      // lib.mkIf cfg.enablePlymouth {
+        # Enable Plymouth if the option is set
+        boot = {
+          plymouth.enable = true;
+          initrd.systemd.enable = true;
+          kernelParams = ["quiet"];
         };
       };
-    } // lib.mkIf cfg.enablePlymouth {
-      # Enable Plymouth if the option is set
-      boot = {
-        plymouth.enable = true;
-        initrd.systemd.enable = true;
-        kernelParams = [ "quiet" ];
-      };
-    };
   };
 }
