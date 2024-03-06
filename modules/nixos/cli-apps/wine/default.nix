4b19ed2052fe5168599e5a7cb424f5c3bffb52aa
jk <joachim.dev@pm.me>
2024-03-06 08:23:17 +0100

    update wine module to also optionally include bottles

--- modules/nixos/cli-apps/wine/default.nix
+++ modules/nixos/cli-apps/wine/default.nix
@@ -1,20 +1,23 @@
-{ options, config, lib, pkgs, ... }:
-
-with lib;
-with lib.plusultra;
-let
-  cfg = config.plusultra.cli-apps.wine;
-in
 {
+  options,
+  config,
+  lib,
+  pkgs,
+  channels,
+  ...
+}:
+with lib;
+with lib.plusultra; let
+  cfg = config.plusultra.cli-apps.wine;
+in {
   options.plusultra.cli-apps.wine = with types; {
     enable = mkBoolOpt false "Whether or not to enable Wine.";
+    enableBottles = mkBoolOpt false "Whether or not to enable Wine bottles.";
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      winePackages.unstable
-      winetricks
-      wine64Packages.unstable
-    ];
+    environment.systemPackages = with pkgs;
+      [winePackages.unstable winetricks wine64Packages.unstable]
+      ++ (lib.optionals cfg.enableBottles [channels.unstable.bottles]);
   };
 }
