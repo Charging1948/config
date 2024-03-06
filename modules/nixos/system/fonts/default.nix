e1783528ad1ec39503c9a7cf7e974905b1cd3a10
jk <joachim.dev@pm.me>
2024-03-06 08:25:06 +0100

    update font module

--- modules/nixos/system/fonts/default.nix
+++ modules/nixos/system/fonts/default.nix
@@ -1,12 +1,29 @@
-{ options, config, pkgs, lib, ... }:
-with lib;
-with lib.plusultra;
-let cfg = config.plusultra.system.fonts;
-in
 {
+  options,
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with lib;
+with lib.plusultra; let
+  cfg = config.plusultra.system.fonts;
+in {
   options.plusultra.system.fonts = with types; {
     enable = mkBoolOpt false "Whether or not to manage fonts.";
-    fonts = mkOpt (listOf package) [ ] "Custom font packages to install.";
+    fonts = mkOpt (listOf package) [] "Custom font packages to install.";
+    font = {
+      regular = {
+        family = mkOpt str "Fira Sans" "The regular font family.";
+        package = mkOpt package pkgs.fira "The regular font package.";
+      };
+      monospace = {
+        family = mkOpt str "FiraCode Nerd Font" "The monospace font family.";
+        package =
+          mkOpt package pkgs.nerdfonts.override {fonts = ["FiraCode"];}
+          "The monospace font package.";
+      };
+    };
   };
 
   config = mkIf cfg.enable {
@@ -15,7 +32,7 @@ in
       LOG_ICONS = "true";
     };
 
-    environment.systemPackages = with pkgs; [ font-manager ];
+    environment.systemPackages = with pkgs; [font-manager];
 
     fonts.packages = with pkgs;
       [
@@ -23,6 +40,7 @@ in
         noto-fonts-cjk-sans
         noto-fonts-cjk-serif
         noto-fonts-emoji
+        fira
         (nerdfonts.override {
           fonts = [
             "Hack"
@@ -36,6 +54,7 @@ in
             "Terminus"
           ];
         })
-      ] ++ cfg.fonts;
+      ]
+      ++ cfg.fonts;
   };
 }
